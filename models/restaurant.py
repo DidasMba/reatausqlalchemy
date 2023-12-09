e93e49501023a5b22a1389a8bfa15a36e63fd59f
DidasMba <daniel.mbarushi@gmail.com>
2023-12-09 17:53:58 +0300

    restaurant edit

--- models/restaurant.py
+++ models/restaurant.py
@@ -2,3 +2,27 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship('Review', back_populates='restaurant')
+    customers = relationship('Customer', secondary='reviews', back_populates='restaurants')
+
+    @classmethod
+    def fanciest(cls):
+        # Implementation to find and return the fanciest restaurant
+        pass
+
+    def all_reviews(self):
+        # Implementation to get all reviews for this restaurant
+        pass
