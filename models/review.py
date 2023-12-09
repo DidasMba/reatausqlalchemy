b3816254e9303b85ac9d2d60eb1faf8c3e046851
DidasMba <daniel.mbarushi@gmail.com>
2023-12-09 18:13:32 +0300

    review add

--- models/review.py
+++ models/review.py
@@ -2,3 +2,24 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from .base import Base
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+    restaurant = relationship('Restaurant', back_populates='reviews')
+    customer = relationship('Customer', back_populates='reviews')
+
+    def full_review(self):
+        # Implementation to return a formatted string for the review
+        pass
